d1d895bcd24a1c8f7a8a61e6601ab6e0f63072aa
sahil464-web <147433772+sahil464-web@users.noreply.github.com>
2023-10-14 13:03:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentInput = "";
+let result = 0;
+let operator = "";
+
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById("display").value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        result = eval(currentInput);
+        document.getElementById("display").value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
